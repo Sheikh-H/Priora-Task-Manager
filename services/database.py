1ae18488e06e6ca1da9431046f2c01e554e748d5
Sheikh-H <sheikh.hussain1155@gmail.com>
2026-07-22 18:34:37 +0100

    updates made - improved initialiser functions

--- services/database.py
+++ services/database.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 
 def connect_database():
+    os.makedirs("instance", exist_ok=True)
     connection = sqlite3.connect("instance/task-manager.db")
     connection.row_factory = sqlite3.Row
     connection.execute("PRAGMA foreign_keys = ON;")
@@ -16,33 +17,33 @@ def initialise_database():
     connection = connect_database()
     try:
         connection.execute("""
-                           CREATE TABLE IF NOT EXISTS "users" (
-                               "id"	INTEGER NOT NULL UNIQUE,
-                               "first_name"	TEXT NOT NULL,
-                               "last_name"	TEXT NOT NULL,
-                               "email"	TEXT NOT NULL UNIQUE,
-                               "password"	TEXT NOT NULL,
-                               PRIMARY KEY("id" AUTOINCREMENT)
-                               );
-                               """)
+                        CREATE TABLE IF NOT EXISTS "users" (
+                            "id"	INTEGER NOT NULL UNIQUE,
+                            "first_name"	TEXT NOT NULL,
+                            "last_name"	TEXT NOT NULL,
+                            "email"	TEXT NOT NULL UNIQUE,
+                            "password"	TEXT NOT NULL,
+                            PRIMARY KEY("id" AUTOINCREMENT)
+                            );
+                            """)
         connection.commit()
 
         connection.execute("""
-                           CREATE TABLE IF NOT EXISTS "tasks" (
-                               "id"	INTEGER NOT NULL UNIQUE,
-                               "user_id"	INTEGER NOT NULL,
-                               "title"	TEXT NOT NULL,
-                               "description"	TEXT NOT NULL,
-                               "due_date"	TEXT NOT NULL,
-                               "due_time"	TEXT NOT NULL,
-                               "completion_date"	TEXT NOT NULL,
-                               "completion_time"	INTEGER NOT NULL,
-                               "completed"	INTEGER NOT NULL DEFAULT 0,
-                               PRIMARY KEY("id" AUTOINCREMENT),
-                               FOREIGN KEY("user_id") 
-                               REFERENCES "users"("id")
-                               ON DELETE CASCADE
-                               );""")
+                        CREATE TABLE IF NOT EXISTS "tasks" (
+                            "id"	INTEGER NOT NULL UNIQUE,
+                            "user_id"	INTEGER NOT NULL,
+                            "title"	TEXT NOT NULL,
+                            "description"	TEXT NOT NULL,
+                            "due_date"	TEXT NOT NULL,
+                            "due_time"	TEXT NOT NULL,
+                            "completion_date"	TEXT NOT NULL,
+                            "completion_time"	INTEGER NOT NULL,
+                            "completed"	INTEGER NOT NULL DEFAULT 0,
+                            PRIMARY KEY("id" AUTOINCREMENT),
+                            FOREIGN KEY("user_id") 
+                            REFERENCES "users"("id")
+                            ON DELETE CASCADE
+                            );""")
         connection.commit()
     except Exception as e:
         print(e)
