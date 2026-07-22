b4754ce7407f1d915a25655baec30b75b2c360d0
Sheikh-H <sheikh.hussain1155@gmail.com>
2026-07-22 16:40:56 +0100

    exploring idea's with the login page

--- app.py
+++ app.py
@@ -50,6 +50,13 @@ def login():
     return render_template("user/login.html", title=title, description=description)
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    description = "Sign up page"
+    title = "Register an account"
+    return render_template("user/register.html", title=title, description=description)
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
